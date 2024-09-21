41f6c1ec8a4c436dda90ad50a45a5e14b05f7ba2
alexfleury <28400108+alexfleury@users.noreply.github.com>
2024-09-21 11:50:26 -0400

    beautify borg scripts.

--- bin/backup_perso.sh
+++ bin/backup_perso.sh
@@ -2,7 +2,7 @@
 
 # Some helpers and error handling.
 info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
-trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+trap "echo $( date ) Backup interrupted >&2; exit 2" INT TERM
 
 # Close if borg or rclone is running.
 if pgrep "borg" || pgrep "rclone" > /dev/null
@@ -15,7 +15,7 @@ fi
 # Setting this, so the repo does not need to be given on the commandline.
 export BORG_REPO="/mnt/w/Backups"
 
-# Setting this, so you won't be asked for your repository passphrase.
+# Setting this, so you won"t be asked for your repository passphrase.
 export BORG_PASSPHRASE=$(<~/.borg_pass)
 
 info "Starting backup..."
@@ -40,7 +40,7 @@ borg create                                      \
     --exclude '/mnt/d/$RECYCLE.BIN'              \
     --exclude "/mnt/d/System Volume Information" \
                                                  \
-    ::'{hostname}-{now}'                         \
+    ::"{hostname}-{now}"                         \
     "/mnt/d"
 
 backup_exit=$?
@@ -48,13 +48,13 @@ backup_exit=$?
 info "Pruning repository..."
 
 # Use the `prune` subcommand to maintain d daily, w weekly and m monthly
-# archives of THIS machine. The '{hostname}-' prefix is very important to
-# limit prune's operation to this machine's archives and not apply to
-# other machines' archives also:
+# archives of THIS machine. The "{hostname}-" prefix is very important to
+# limit prune"s operation to this machine"s archives and not apply to
+# other machines" archives also:
 
 borg prune                  \
     --list                  \
-    --prefix '{hostname}-'  \
+    --prefix "{hostname}-"  \
     --show-rc               \
     --keep-daily    1       \
     --keep-weekly   1       \
